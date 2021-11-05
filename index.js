d4a72e6e6ba8bd7281bce1f68e1f0c92a464f634
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-11-05 14:51:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const { response } = require('express')
+const express = require('express')
+require('dotenv').config()
+const db = require('./helper/db')
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (request, response) => {
+    response.send('Backend Project Node Express')
+})
+
+app.get('/project', (req, res) => {
+    db.query('SELECT * FROM project', (err, result, fields) => {
+        if(!err) {
+            if(result.length) {
+                res.status(200).send({
+                    success: true,
+                    message: 'Project List',
+                    data: result
+                })
+            } else {
+                res.status(404).send({
+                    success: false,
+                    message: 'Item project not found!'
+                })
+            }
+        } else {
+            res.status(500).send({
+                success: false,
+                message: 'Internal Server Error!'
+            })
+        }
+        
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Listen app backend on port ${port}`)
+})
