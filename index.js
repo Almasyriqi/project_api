8ead60e1aa33827464a370021a70145bc46ea026
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-11-06 22:24:02 +0700

    Mengubah format pesan json

--- index.js
+++ index.js
@@ -17,22 +17,22 @@ app.get('/project', (req, res) => {
     db.query('SELECT * FROM project', (err, result, fields) => {
         if(!err) {
             if(result.length) {
-                res.status(200).send({
+                res.status(200).send([{
                     success: true,
                     message: 'Project List',
                     data: result
-                })
+                }])
             } else {
-                res.status(404).send({
+                res.status(404).send([{
                     success: false,
                     message: 'Item project not found!'
-                })
+                }])
             }
         } else {
-            res.status(500).send({
+            res.status(500).send([{
                 success: false,
                 message: 'Internal Server Error!'
-            })
+            }])
         }
         
     })
@@ -40,27 +40,29 @@ app.get('/project', (req, res) => {
 
 app.post('/project', (req, res) => {
 
-    const {projectName, projectDesc, projectType} = req.body
+    const projectName = req.body.project_name;
+    const projectDesc = req.body.project_desc;
+    const projectType = req.body.project_type;
 
     db.query(`INSERT INTO project (project_name, project_desc, project_type) 
     VALUES ('${projectName}', '${projectDesc}', '${projectType}')`, (err, result, fields) => {
         if(!err) {
             if(result.affectedRows) {
-                res.status(200).send({
+                res.status(200).send([{
                     success: true,
                     message: 'Success add project!'
-                })
+                }])
             } else {
-                res.status(400).send({
+                res.status(400).send([{
                     success: false,
                     message: 'Submit project failed!'
-                })
+                }])
             }
         } else {
-            res.status(500).send({
+            res.status(500).send([{
                 success: false,
                 message: 'Internal Server Error!'
-            })
+            }])
         }
     })
 })
@@ -72,22 +74,22 @@ app.delete('/project/:projectId', (req, res) => {
         if (result.length) {
             db.query(`DELETE FROM project WHERE project_id = ${projectId}`, (err, result, fields) => {
                 if(result.affectedRows) {
-                    res.status(200).send({
+                    res.status(200).send([{
                         success: true,
                         message: `Item project id ${projectId} has been deleted!`
-                    })
+                    }])
                 } else {
-                    res.status(404).send({
+                    res.status(404).send([{
                         success: false,
                         message: 'Item project failed to delete'
-                    })
+                    }])
                 }
             })
         } else {
-            res.status(404).send({
+            res.status(404).send([{
                 success: false,
                 message: 'Data project not found!'
-            })
+            }])
         }
     })
 })
@@ -98,22 +100,22 @@ app.get('/project/:projectId', (req, res) => {
     db.query(`SELECT * FROM project WHERE project_id = ${projectId}`, (err, result, fields) => {
         if(!err) {
             if(result.length) {
-                res.status(200).send({
+                res.status(200).send([{
                     success: true,
                     message: `Project with id = ${projectId}`,
                     data: result
-                })
+                }])
             } else {
-                res.status(404).send({
+                res.status(404).send([{
                     success: false,
                     message: 'Item project not found!'
-                })
+                }])
             }
         } else {
-            res.status(500).send({
+            res.status(500).send([{
                 success: false,
                 message: 'Internal Server Error!'
-            })
+            }])
         }
         
     })
